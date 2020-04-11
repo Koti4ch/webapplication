11ba180793ce7c207753db8c3468d2d104a50cc1
Rostislav <koti4Matroskin@gmail.com>
2020-04-11 12:43:34 +0300

    added custom fields to User and register in adminpanel

--- authuser/admin.py
+++ authuser/admin.py
@@ -1,8 +1,36 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
-from .models import User
+from django.conf import settings
+from .models import PersonalUserInfo, User
+
+
+
+# @admin.register(User)
+# class UserAdminModel(admin.ModelAdmin):
+    # readonly_fields = ('rmg_to_reg', 'login_to_reg')
+
+class UserInfoInline(admin.StackedInline):
+    model = PersonalUserInfo
+    can_delete = False
+    verbose_name_plural = 'Personal info'
+
+    fields = ()
+    list_display = ('radio_chanal',)
 
 @admin.register(User)
 class UserAdminModel(admin.ModelAdmin):
+    inlines = [UserInfoInline,]
     readonly_fields = ('rmg_to_reg', 'login_to_reg')
+
+    fields = ()
+    list_display = ('username', 'radio_chanal_list', 'is_active')
+
+    # 
+    def radio_chanal_list(self, obj):
+        shortname = obj.personaluserinfo.radio_chanal
+        return obj.personaluserinfo.find_choice(shortname)
+    radio_chanal_list.short_description = 'Radio chanal'
+
+# admin.site.unregister(settings.AUTH_USER_MODEL)
+# admin.site.register(settings.AUTH_USER_MODEL, UserAdminModel)
--- authuser/models.py
+++ authuser/models.py
@@ -1,11 +1,57 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 import os, socket
+
 # Create your models here.
 class User(AbstractUser):
     rmg_to_reg = models.CharField(max_length=25, blank=False, null=False, default=socket.gethostname, verbose_name='Created from compname')
     last_rmg_used = models.CharField(max_length=25, blank=True, null=True, verbose_name='Last connect from')
     login_to_reg = models.CharField(max_length=50, default=os.getlogin, blank=False, null=False, verbose_name='Created by login')
 
-    # def comp_name()
+
+
+class PersonalUserInfo(models.Model):
+    RADIO_CHANALS = (
+        ('1k', '1й Канал'),
+        ('st', 'Столица'),
+        ('kl', 'Культура'),
+        ('ra', 'Радиус ФМ'),
+        ('na', 'Не определилась'),
+    )
+
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    radio_chanal = models.CharField(max_length=2, choices=RADIO_CHANALS, default='na')
+    radio_room = models.CharField(max_length=9, blank=True)
+    working_position = models.CharField(max_length=25, blank=True)
+    user_birthday = models.DateField(null=True, blank=True)
+    user_telephone = models.CharField(max_length=15, blank=True)
+    user_about = models.TextField(max_length=255, blank=True)
+    # friendlist = 
+
+    def __str__(self):
+        return 'about {}'.format(self.user.username)
+    
+    def find_choice(self, shortname):
+        for _ in self.RADIO_CHANALS:
+            if _[0] == shortname:
+                return _[1]
+    
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_addition_info(sender, instance, created, **kwargs):
+    if created:
+        PersonalUserInfo.objects.create(user=instance)
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def resave_userinfo(sender, instance, **kwargs):
+    instance.personaluserinfo.save()
+
+
+
+
