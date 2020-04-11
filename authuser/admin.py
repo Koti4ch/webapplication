37f41ced368ebd5c1498dc84830b30a651a8080f
Rostislav <koti4Matroskin@gmail.com>
2020-04-11 19:45:50 +0300

    update UserAdminModel added filters, search...

--- authuser/admin.py
+++ authuser/admin.py
@@ -15,8 +15,9 @@ class UserInfoInline(admin.StackedInline):
     can_delete = False
     verbose_name_plural = 'Personal info'
 
-    fields = ()
-    list_display = ('radio_chanal',)
+    # radio_fields = {'radio_chanal': admin.VERTICAL}
+
+
 
 @admin.register(User)
 class UserAdminModel(admin.ModelAdmin):
@@ -24,13 +25,18 @@ class UserAdminModel(admin.ModelAdmin):
     readonly_fields = ('rmg_to_reg', 'login_to_reg')
 
     fields = ()
+    exclude = ('password', 'groups', 'user_permissions')
     list_display = ('username', 'radio_chanal_list', 'is_active')
+
+    list_filter = ('personaluserinfo__radio_chanal', 'is_active',)
+    search_fields = ('username',)
 
     # 
     def radio_chanal_list(self, obj):
         shortname = obj.personaluserinfo.radio_chanal
         return obj.personaluserinfo.find_choice(shortname)
     radio_chanal_list.short_description = 'Radio chanal'
+    radio_chanal_list.admin_order_field = 'personaluserinfo__radio_chanal'
 
 # admin.site.unregister(settings.AUTH_USER_MODEL)
 # admin.site.register(settings.AUTH_USER_MODEL, UserAdminModel)
