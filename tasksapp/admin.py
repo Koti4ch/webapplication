f5c0731ea075d4c912a393ce8e3bcae7f47b7ff4
Rost <Koti4Matroskin@gmail.com>
2020-08-12 13:01:19 +0300

    added admin fields and model for tasklistdev

--- tasksapp/admin.py
+++ tasksapp/admin.py
@@ -4,4 +4,8 @@ from .models import Task
 
 @admin.register(Task)
 class TaskAdminPanel(admin.ModelAdmin):
-    readonly_fields = ['task_title', 'task_msg']
+    # readonly_fields = ['task_slug',]
+    prepopulated_fields = {'task_slug': ('open_by', 'task_title',), }
+    list_display = ('task_title', 'open_by', 'closed_by', 'task_status', 'create_time')
+    list_filter = ('task_status',)
+    ordering = ('create_time', 'task_title')
--- tasksapp/models.py
+++ tasksapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 from django.utils import timezone
 from django.conf import settings
 # Create your models here.
@@ -9,19 +10,24 @@ class Task(models.Model):
     Model for tasks
     '''
     task_title = models.CharField(max_length=50, blank=False, null=False, verbose_name='Заголовок задания', help_text='Не более 50 символов')
-    task_msg = models.CharField(max_length=800,verbose_name='Описание задания', blank=False, null=False, help_text='')
+    task_slug = models.SlugField(
+        max_length=50, blank=False, null=False, verbose_name='task slug title')
+    task_msg = models.TextField(max_length=255,verbose_name='Описание задания', blank=False, null=False, help_text='')
 
     STATUSES = (
-        (1, 'Открыто'),
-        (2, 'В процессе выполнения'),
-        (3, 'Выполнено'),
-        (0, 'Закрыто'),
+        ('1', 'Открыто'),
+        ('2', 'В процессе выполнения'),
+        ('3', 'Выполнено'),
+        ('0', 'Закрыто'),
     )
 
     task_status = models.CharField(max_length=1, blank=False, null=False, choices=STATUSES, default=STATUSES[0], verbose_name='Состояние заявки', help_text='')
-    # TODO: подумать какие тут связи будут!
-    who_open = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    who_close = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # related_name обязательно, когда надо в форенкея.
+    open_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='open_by')
+    closed_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='closed_by', null=True, blank=True)
 
-    create_time = models.DateTimeField(default=timezone.now, blank=True, null=False)
-    closed_time = models.DateTimeField(default='', blank=True, null=False)
+    create_time = models.DateTimeField(default=timezone.datetime.now, blank=True, null=False)
+    closed_time = models.DateTimeField(blank=True, null=True)
+
+    #def __str__(self):
+    #    return task_title
