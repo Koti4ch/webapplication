779b52069ae55d2f56f53d9cc62a02da6c6329ee
KotAtWork <Koti4Matroskin@gmail.com>
2020-09-14 12:40:59 +0300

    added max correct answers and validate formset in adminpanel

--- quizzy/admin.py
+++ quizzy/admin.py
@@ -1,16 +1,38 @@
 from django.contrib import admin
+from django.forms.models import BaseInlineFormSet
+from django.forms.forms import ValidationError
+from django.db.models import Avg, Count, Min, Sum
 from quizzy.models import Question, Ask
 # Register your models here.
+
+class OnlyOneValidate(BaseInlineFormSet):
+    '''
+    Валидируем на кол-во верных ответов
+    '''
+    def clean(self):
+        checkbox = 0
+        for form in self.forms:
+            max_correct_answers = self.instance.max_correct_answers
+            try:
+                if form.cleaned_data.get('is_correct') == True:
+                    checkbox += 1
+            except AttributeError:
+                print('AttributeError')
+                pass
+        print(checkbox)
+        if checkbox != int(max_correct_answers):
+            raise ValidationError(f'Для этого вопроса должно быть выбрано {max_correct_answers} правильных ответов!')
 
 
 class AskInline(admin.StackedInline):
     model = Ask
     fields = (('ask', 'is_correct'), )
+    formset = OnlyOneValidate
     can_delete = True
     extra = 1
     min_num = None
-    verbose_name = 'Ответ'
-    verbose_name_plural = 'Ответы'
+    verbose_name = 'ответ'
+    verbose_name_plural = 'ответы'
 
 
 @admin.register(Question)
@@ -21,3 +43,14 @@ class QuestionAdmin(admin.ModelAdmin):
     def allanswers(self, obj):
         return Ask.objects.filter(question_id=obj.id).count()
     allanswers.short_description = 'Всего ответов'
+
+################################## TEST             #######################
+@admin.register(Ask)
+class AskAdmin(admin.ModelAdmin):
+    list_display = ('aaaaaaa',)
+    counter = 0
+
+    def aaaaaaa(self, obj):
+        self.counter += 1
+        print(self.counter)
+        return f'#{self.counter}'
--- quizzy/models.py
+++ quizzy/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Avg, Count, Min, Sum
 # Create your models here.
 
 
@@ -15,9 +16,14 @@ class Question(models.Model):
         ("znak", "Знаки"),
     )
 
+    nums = (
+        (str(_), str(_)) for _ in range(1, 10)
+    )
+
     description = models.CharField("Вопрос", max_length=300, null=False, blank=False, unique=True)
     difficulty = models.CharField("Сложность", max_length=1, choices=rang, default=rang[1][0])
     direction = models.CharField("Направление", max_length=10, choices=direct, default=direct[0][0])
+    max_correct_answers = models.CharField("Количество правильных ответов на вопрос", max_length=1, choices=nums, default=rang[1][0])
 
     def __str__(self):
         return f'{self.description[:10]}...'
@@ -27,11 +33,11 @@ class Question(models.Model):
         verbose_name_plural = 'Вопросы'
 
 
-
 class Ask(models.Model):
     question = models.ForeignKey('question', on_delete=models.CASCADE)
     ask = models.CharField("Ответ", max_length=300, null=False, blank=False)
     is_correct = models.BooleanField("Верно", default=False)
 
+#   TODO: think how create show 1,2,3,4,5,6, etc
     def __str__(self):
-        return f'{self.question_id}'
+        return f'#{self.id}'
